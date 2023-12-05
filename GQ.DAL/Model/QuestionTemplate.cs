dfe499e7e766f31fb62c5b046aa231d4b36a104f
camar-1 <camar.2001bk@gmail.com>
2023-12-05 20:33:57 +0600

    added  Path for connection to Database

--- GQ.DAL/Model/QuestionTemplate.cs
+++ GQ.DAL/Model/QuestionTemplate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GQ.DAL.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ GQ.DAL/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GQ.DAL
+{
+    public class Repository
+    {
+        private string _Path = @"C:\Users\TEMP\GeneratorQuestions.db";
+
+        public Repository(string path)
+        {
+            _Path = path;
+        }
+    }
+}
